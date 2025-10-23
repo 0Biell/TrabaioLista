116609d91f979fc4626302c8d1a144d2f00b4e2a
joaoCanonica <jcanonicaescola@gmail.com>
2025-10-23 14:33:42 -0300

    trabaio

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace lista
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Lista = new System.Windows.Forms.ListBox();
+            this.valor = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // Lista
+            // 
+            this.Lista.FormattingEnabled = true;
+            this.Lista.Location = new System.Drawing.Point(109, 127);
+            this.Lista.Name = "Lista";
+            this.Lista.Size = new System.Drawing.Size(263, 160);
+            this.Lista.TabIndex = 0;
+            // 
+            // valor
+            // 
+            this.valor.Location = new System.Drawing.Point(109, 83);
+            this.valor.Name = "valor";
+            this.valor.Size = new System.Drawing.Size(180, 20);
+            this.valor.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(314, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.enterPressed);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(125, 293);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(225, 47);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(357, 98);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.valor);
+            this.Controls.Add(this.Lista);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox Lista;
+        private System.Windows.Forms.TextBox valor;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace lista
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+         
+        private void addItemsToList(string item)
+        {
+            string i = item.Trim();
+            if(i != "")
+            {
+                Lista.Items.Add(i);
+            }
+
+
+            
+        }
+        private void enterPressed(object sender,System.Windows.Forms.KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+               addItemsToList(valor.Text);
+                valor.Text = " ";
+            } else if(e.KeyCode == Keys.Delete)
+            {
+                if (Lista.SelectedItem != null)
+                {
+                    Lista.Items.Remove(Lista.SelectedItem);
+                }
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            addItemsToList(valor.Text);
+            valor.Text = "";
+        }
+
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Lista.SelectedItem != null)
+            {
+                Lista.Items.Remove(Lista.SelectedItem);
+            }
+        }
+    }
+}
